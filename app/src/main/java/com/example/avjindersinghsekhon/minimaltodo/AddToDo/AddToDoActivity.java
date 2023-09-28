3c766ca2e54542b1e114e6c8a7de77ed71b5f87f
Jieun Lim <jieun776121@gmail.com>
2023-09-28 18:32:56 +0900

    bring imageUri from AddToDoFragment

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddToDo/AddToDoActivity.java
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddToDo/AddToDoActivity.java
@@ -1,8 +1,14 @@
 package com.example.avjindersinghsekhon.minimaltodo.AddToDo;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.avjindersinghsekhon.minimaltodo.AppDefault.AppDefaultActivity;
 import com.example.avjindersinghsekhon.minimaltodo.R;
@@ -28,6 +34,34 @@ public class AddToDoActivity extends AppDefaultActivity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.e("IMJE", "requestCode: "+ requestCode);
+        Log.e("IMJE", "resultCode: "+ resultCode);
+
+        if (resultCode == RESULT_OK) {
+            Uri photoUri = data.getData();
+            Log.e("photoUri", photoUri.toString());
+
+            Cursor cursor = getContentResolver().query(photoUri, null, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+                String mediaPath = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                cursor.close();
+                Log.e("경로 확인 >> ", photoUri.toString() + " + " + mediaPath);
+
+                AddToDoFragment addToDoFragment = (AddToDoFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+//                if (addToDoFragment != null) {
+//                    addToDoFragment.setImageWithMediaPath(mediaPath);
+//                }
+            }
+        } else {
+            Toast.makeText(this, "사진 업로드 실패", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
